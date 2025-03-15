8580bb6457a20faec2256008eaf447d1c448d881
Mrking671 <telegrambot969797@gmail.com>
2025-03-15 14:46:41 +0530

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react';
+import Link from 'next/link';
+
+export default function Home() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch('https://your-backend-domain.com/movies')
+      .then((res) => res.json())
+      .then((data) => setMovies(data))
+      .catch((err) => console.error(err));
+  }, []);
+
+  return (
+    <div style={{ padding: '1rem' }}>
+      <h1>Movie Streaming Site</h1>
+      <ul>
+        {movies.map((movie) => (
+          <li key={movie._id}>
+            <Link href={`/movie/${movie._id}`}>
+              <a>{movie.file_name}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
